341f398b5184035507ecab8f7b3c05705081e66f
Kanisevsky <meltsmokestudio@gmail.com>
2024-07-29 19:53:11 +0100

    organization

--- app/(platform)/(dashboard)/_components/Navbar.tsx
+++ app/(platform)/(dashboard)/_components/Navbar.tsx
@@ -1,15 +1,25 @@
+import { Plus } from 'lucide-react';
 import { Logo } from '@/components/logo';
-import React from 'react';
+import { Button } from '@/components/ui/button';
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 z-50 w-full h-14 border-b shadow-sm bg-white flex items-center">
+    <nav className="fixed top-0 px-4 z-50 w-full h-14 border-b shadow-sm bg-white flex items-center">
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex ">
           <Logo />
         </div>
+        <Button
+          size="sm"
+          className="rounded-sm hidden md:block h-auto py-1.5 px-2"
+        >
+          Create
+        </Button>
+        <Button size="sm" className="rounded-sm block md:hidden">
+          <Plus className="h-4 w-4" />
+        </Button>
       </div>
-    </div>
+    </nav>
   );
 };
 
