713964d18d44b54eb2eaec2990cd8c5c76615eb7
Kanisevsky <meltsmokestudio@gmail.com>
2024-07-30 15:46:56 +0100

    sidebar

--- app/(platform)/(dashboard)/_components/Sidebar.tsx
+++ app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -5,6 +5,9 @@ import { Plus } from 'lucide-react';
 import { useLocalStorage } from 'usehooks-ts';
 import { useOrganization, useOrganizationList } from '@clerk/nextjs';
 import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Accordion } from '@/components/ui/accordion';
 interface SidebarProps {
   storageKey?: string;
 }
