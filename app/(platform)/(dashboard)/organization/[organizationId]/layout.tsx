06d718939d71b5b1fb2005de64403d623413aced
Kanisevsky <meltsmokestudio@gmail.com>
2024-07-30 15:36:18 +0100

    sidebar

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,12 @@
+import OrgControl from './_components/OrgControl';
+
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <OrgControl />
+      {children}
+    </div>
+  );
 };
 
 export default OrganizationIdLayout;
