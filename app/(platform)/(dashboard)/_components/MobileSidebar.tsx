85616662a30863d90ce8c744b8022bbeb09da22f
Kanisevsky <meltsmokestudio@gmail.com>
2024-07-31 17:14:27 +0100

    sidebar

---
+++ app/(platform)/(dashboard)/_components/MobileSidebar.tsx
@@ -0,0 +1,23 @@
+'use client';
+import { useMobileSidebar } from '@/hooks/use-mobile-sidebar';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+const MobileSidebar = () => {
+  const pathname = usePathname();
+  const [isMounted, setIsMounted] = useState(false);
+  const onOpen = useMobileSidebar((state) => state.onOpen);
+  const onClose = useMobileSidebar((state) => state.onClose);
+  const isOpen = useMobileSidebar((state) => state.isOpen);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return <div>Mobile Sidebar</div>;
+};
+
+export default MobileSidebar;
